06d1eca5cad813d66c1ee4b25feaee86de9336d1
dwils121 <dwils121@kent.edu>
2025-02-13 16:15:58 -0500

    roxygen edits

--- R/rnormmixcode.R
+++ R/rnormmixcode.R
@@ -1,15 +1,30 @@
-pnormmix = function(q, mean1, sd1, mean2, sd2){(2/3)*pnorm(q,mean1,sd1) +
-    (1/3)*pnorm(q,mean2,sd2)}
+#'
+#' @param q description
+#' @param
+#' @param
+#' @param
+#' @param
+#' @param
+#' @param
+#'
+#'
+#' @return
+#' @export
+#'
+#' @example
 
-dnormmix = function(x, mean1, sd1, mean2, sd2){(2/3)*dnorm(x,mean1,sd1) +
+pnormmix = function(q, mean1, sd1, mean2, sd2, mixprop){(1-mixprop)*pnorm(q,mean1,sd1) +
+    (mixprop)*pnorm(q,mean2,sd2)}
+
+dnormmix = function(x, mean1, sd1, mean2, sd2, mixprop){(1-mixprop)*dnorm(x,mean1,sd1) +
     (1/3)*dnorm(x,mean2,sd2)}
 
-qnormmix = function(p, mean1, sd1, mean2, sd2){
-  fun = function(z){pnormmix(z, mean1, sd1, mean2, sd2)-p}
+qnormmix = function(p, mean1, sd1, mean2, sd2, mixprop){
+  fun = function(z){pnormmix(z, mean1, sd1, mean2, sd2, mixprop)-p}
   uniroot(fun, interval = c(-10,10), extendInt = "yes")$root
 }
 qnormmix = Vectorize(qnormmix)
 
-rnormmix = function(n, mean1, sd1, mean2, sd2) {
-  ifelse(rbinom(n=n,size=1,prob=2/3)==1, rnorm(n,mean1,sd1), rnorm(n,mean2,sd2))
+rnormmix = function(n, mean1, sd1, mean2, sd2, mixprop) {
+  ifelse(rbinom(n=n,size=1,prob=1-mixprop)==1, rnorm(n,mean1,sd1), rnorm(n,mean2,sd2))
 }
