819f1d99682b0b3221c633e99e052ac3606c56c2
dwils121 <dwils121@kent.edu>
2025-02-13 18:27:35 -0500

    package update

--- R/rnormmixcode.R
+++ R/rnormmixcode.R
@@ -1,12 +1,14 @@
+#' Mixture of Normal Distributions
 #'
-#' @param q description
-#' @param
-#' @param
-#' @param
-#' @param
-#' @param
-#' @param
-#'
+#' @param q A given quantile for the CDF.
+#' @param x A given quantile for the PDF.
+#' @param p A given probability for the inverse CDF.
+#' @param n Number of generated numbers from the distribution.
+#' @param mean1 Mean for the first normal distribution.
+#' @param mean2 Mean for the second normal distribution.
+#' @param sd1 Standard deviation for the first normal distribution.
+#' @param sd2 Standard deviation for the second normal distribution.
+#' @param mixprop Proportion that the second normal distribution holds in the mixture.
 #'
 #' @return
 #' @export
@@ -17,7 +19,7 @@ pnormmix = function(q, mean1, sd1, mean2, sd2, mixprop){(1-mixprop)*pnorm(q,mean
     (mixprop)*pnorm(q,mean2,sd2)}
 
 dnormmix = function(x, mean1, sd1, mean2, sd2, mixprop){(1-mixprop)*dnorm(x,mean1,sd1) +
-    (1/3)*dnorm(x,mean2,sd2)}
+    mixprop*dnorm(x,mean2,sd2)}
 
 qnormmix = function(p, mean1, sd1, mean2, sd2, mixprop){
   fun = function(z){pnormmix(z, mean1, sd1, mean2, sd2, mixprop)-p}
